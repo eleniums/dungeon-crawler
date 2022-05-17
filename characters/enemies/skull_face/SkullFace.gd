96f3b924b0003fa079f730c66283d27824a81849
eleniums <eleniums@yahoo.com>
2022-05-17 13:38:26 -0700

    Add weapon and allow enemy to die

--- characters/enemies/skull_face/SkullFace.gd
+++ characters/enemies/skull_face/SkullFace.gd
@@ -7,6 +7,8 @@ export var TOUCH_DAMAGE = 1
 var _velocity = Vector2()
 var _timer = 0
 
+var _hp = 3
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -56,7 +58,14 @@ func handle_movement():
 
 
 func _on_Hitbox_area_entered(area):
-	var dir = Vector2.RIGHT
-	if area.global_position.x < global_position.x:
-		dir = Vector2.LEFT
-	Engine.hurt_player(TOUCH_DAMAGE, dir)
+	if area.is_in_group("player"):
+		var dir = Vector2.RIGHT
+		if area.global_position.x < global_position.x:
+			dir = Vector2.LEFT
+		Engine.hurt_player(TOUCH_DAMAGE, dir)
+	elif area.is_in_group("player_weapon"):
+		_hp -= Engine.weapon_damage
+		print("Monster took " + str(Engine.weapon_damage) + " damage. HP remaining: " + str(_hp))
+		if _hp <= 0:
+			queue_free()
+			print("Monster defeated.")
