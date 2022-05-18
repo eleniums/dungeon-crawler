cff7e6afe043ebc045473834c25caf8d265852a1
Stan Nelson <snelson@miteksystems.com>
2022-05-18 14:05:29 -0700

    Can now fade in and out and determine if fading finished

---
+++ common/Fader.gd
@@ -0,0 +1,39 @@
+extends ColorRect
+
+enum FadeState {
+	NO_FADE,
+	FADE_IN,
+	FADE_TO_BLACK,
+}
+
+signal faded_in
+signal faded_to_black
+
+var _state = FadeState.NO_FADE
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	match _state:
+		FadeState.FADE_TO_BLACK:
+			var result = lerp(get_modulate(), Color(0,0,0,1), 0.2)
+			set_modulate(result)
+			if result == Color(0,0,0,1):
+				emit_signal("faded_to_black")
+		FadeState.FADE_IN:
+			var result = lerp(get_modulate(), Color(0,0,0,0), 0.2)
+			set_modulate(result)
+			if result == Color(0,0,0,0):
+				emit_signal("faded_in")
+
+func fade_to_black():
+	set_modulate(Color(0,0,0,0))
+	_state = FadeState.FADE_TO_BLACK
+	
+func fade_in():
+	set_modulate(Color(0,0,0,1))
+	_state = FadeState.FADE_IN
