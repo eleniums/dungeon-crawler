a24f48e79a7a9de4ac658df29833c4f45b252b37
Stan Nelson <snelson@miteksystems.com>
2022-05-18 15:15:33 -0700

    Add engine method for random numbers

--- common/Engine.gd
+++ common/Engine.gd
@@ -13,17 +13,27 @@ var fader = null
 var arrow = preload("res://weapons/arrow/Arrow.tscn")
 var explosion = preload("res://doodads/explosion/Explosion.tscn")
 
+var rng = RandomNumberGenerator.new()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_node("/root/Main/Players/Player")
 	weapons = get_node("/root/Main/Weapons")
 	explosions = get_node("/root/Main/Explosions")
-	fader = get_node("/root/Main/HUD/Fader")
+	fader = get_node("/root/Main/HUD/Fader")	
+	
+	# used for level generation so seeds can be reproduced
+	rng.randomize() # initialize with a random time-based seed
+	#rng.seed = hash("testseed") # initialize with a user provided seed
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func get_rand(low_inclusive:int, high_inclusive:int):
+	# generates a random number in the given range, inclusive
+	return rng.randi_range(low_inclusive, high_inclusive)
 
 func hurt_player(damage, dir):
 	if !player.is_invincible():
