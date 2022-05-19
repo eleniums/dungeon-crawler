b3401962dfea16581d15987d0ac5c3d3dd62a1ff
eleniums <eleniums@yahoo.com>
2022-05-19 14:42:20 -0700

    Allow exiting game when game over is reached

--- levels/Pause.gd
+++ levels/Pause.gd
@@ -8,15 +8,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	if Engine.current_hp <= 0:
+		return
+	
 	if get_tree().paused:
-		print("game is paused")
 		if Input.is_action_just_released("ui_cancel"):
-			print("unpause game")
 			get_tree().paused = false
 			visible = false
 		elif Input.is_action_just_released("quit"):
 			get_tree().quit()
 	elif Input.is_action_just_released("ui_cancel"):
-		print("pause game")
 		get_tree().paused = true
 		visible = true
