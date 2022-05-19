97c05d13b29c5102b66aa344842d665dddaff235
eleniums <eleniums@yahoo.com>
2022-05-19 15:45:12 -0700

    Start with focus on start button so keys can be used to navigate

--- levels/menu/Menu.gd
+++ levels/menu/Menu.gd
@@ -8,7 +8,7 @@ enum MenuState {
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	$CanvasLayer/MainMenu/VBoxContainer/Start.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
